14663ed9a919c9b212a9e08f356b92dff070c873
ChrisRoach1 <c.roach10@yahoo.com>
2026-05-04 20:48:49 -0400

    lots of stuff

--- app/Services/SocialServiceInterface.php
+++ app/Services/SocialServiceInterface.php
@@ -2,9 +2,13 @@
 
 namespace App\Services;
 
+use App\Models\UserToken;
+
 interface SocialServiceInterface
 {
     public function getPosts();
 
     public function createPost(string $authToken, string $content, ?string $user_token_id = null, ?string $media = null);
+
+    public function refreshToken(UserToken $userToken);
 }
